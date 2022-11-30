7640afca2c282343ddd890e1f99417eb029656f7
Fred Lahode <lahode@infomaniak.ch>
2022-11-30 16:34:17 +0100

    Change app name and ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'fungest-mobile',
+  appId: 'io.ionic.fungestcost',
+  appName: 'fungest-mobile-cost',
   webDir: 'build',
   bundledWebRuntime: false
 };
